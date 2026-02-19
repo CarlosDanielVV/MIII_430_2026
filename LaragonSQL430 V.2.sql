15a5ba651575ede651aad7c596f355340a91e43c
CarlosDanielVV <villanuevavillacarlosdaniel@gmail.com>
2026-02-19 09:31:29 -0600

    Add files via upload

---
+++ LaragonSQL430 V.2.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE Panaderia;
+CREATE DATABASE Escuela;
+CREATE DATABASE Negocio;
+CREATE DATABASE Jugueteria;
+CREATE DATABASE Tienda;
+
+-- Seleccionamos la Base de Datos escuela que vamos a utilizar
+USE Escuela;
+
+CREATE TABLE ALUMNO(
+NC VARCHAR(10),
+Nombre VARCHAR(40),
+APaterno VARCHAR(40),
+AMterno VARCHAR(40),
+Semestre INT,
+Grupo VARCHAR(1),
+Direccion VARCHAR(50),
+Telefono VARCHAR(15),
+PRIMARY KEY (NC)
+);
+CREATE DATABASE Biblioteca;
+USE bibilioteca;
+CREATE TABLE Libro (
+NL INT,
+Titulo VARCHAR(50),
+Edicion VARCHAR(30),
+Fecha VARCHAR(30),
+PRIMARY KEY (NL)
+);
+CREATE TABLE Autor(
+NA INT,
+Nombre VARCHAR(50),
+PaisOrigen VARCHAR(30),
+Genero VARCHAR (30),
+PRIMARY KEY (NA)
+);
+SHOW TABLES;
+
+
+
+
+
+
+
